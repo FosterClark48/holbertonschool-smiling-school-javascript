fbde0c1360f27bab07a4f77cfb156f468cbd8c95
Foster Clark <fosterclark48@yahoo.com>
2023-08-02 01:12:04 -0500

    added scripts to dynamically add popular video section and create carousel from the cards

--- scripts.js
+++ scripts.js
@@ -37,83 +37,76 @@ $(document).ready(function () {
 });
 
 // POPULAR VIDEO SECTION
-function createVideoCard(video) {
-    var card = $('<div>').addClass('card');
-    var img = $('<img>').attr({
-        'src': video.thumb_url,
-        'alt': 'Video thumbnail'
-    }).addClass('card-img-top');
+function createCard(video) {
+    const { thumb_url, title, 'sub-title': sub_title, author_pic_url, author, duration, star } = video;
 
-    var playOverlay = $('<div>').addClass('card-img-overlay text-center');
-    var playButton = $('<img>').attr({
-        'src': 'images/play.png',
-        'alt': 'Play',
-        'width': '64px'
-    }).addClass('align-self-center play-overlay');
-    playOverlay.append(playButton);
-
-    var cardBody = $('<div>').addClass('card-body');
-    var cardTitle = $('<h5>').text(video.title).addClass('card-title font-weight-bold');
-
-    var cardText = $('<p>').text(video['sub-title']).addClass('card-text text-muted');
-
-    var creator = $('<div>').addClass('creator d-flex align-items-center');
-    var creatorImage = $('<img>').attr({
-        'src': video.author_pic_url,
-        'alt': 'Creator of video',
-        'width': '30px'
-    }).addClass('rounded-circle');
-    var creatorName = $('<h6>').text(video.author).addClass('pl-3 m-0 main-color');
-    creator.append(creatorImage, creatorName);
-
-    var info = $('<div>').addClass('info pt-3 d-flex justify-content-between');
-    var rating = $('<div>').addClass('rating');
-    for (var i = 0; i < 5; i++) {
-        var star = $('<img>').attr({
-            'src': (i < video.star ? 'images/star_on.png' : 'images/star_off.png'),
-            'alt': 'star on',
-            'width': '15px'
-        });
-        rating.append(star);
+    let stars = '';
+    for(let i = 1; i <= 5; i++) {
+        if(i <= star) {
+            stars += '<img src="images/star_on.png" alt="star on" width="15px" height="15px">';
+        } else {
+            stars += '<img src="images/star_off.png" alt="star off" width="15px" height="15px">';
+        }
     }
-    var duration = $('<span>').text(video.duration).addClass('main-color');
-    info.append(rating, duration);
 
-    cardBody.append(cardTitle, cardText, creator, info);
-    card.append(img, playOverlay, cardBody);
+    const cardContainer = $('<div>').addClass('col-12 col-sm-6 col-md-4 card-deck');
+    const card = $('<div>').addClass('card border-0 d-flex flex-column').appendTo(cardContainer);
 
-    return $('<div>').addClass('col-sm-6 col-md-6 col-lg-3 d-sm-flex justify-content-md-start justify-content-lg-center').append(card);
+    const thumbnailContainer = $('<div>').addClass('thumbnail-container position-relative').appendTo(card);
+    $('<img>').attr({'src': thumb_url, 'alt': 'Video thumbnail'}).addClass('card-img-top').appendTo(thumbnailContainer);
+
+    const overlay = $('<div>').addClass('card-img-overlay text-center').appendTo(thumbnailContainer);
+    $('<img>').attr({'src': 'images/play.png', 'alt': 'Play Button', 'width': '64px'}).addClass('align-self-center play-overlay').appendTo(overlay);
+
+    const cardBody = $('<div>').addClass('card-body px-2').appendTo(card);
+    $('<h5>').addClass('card-title font-weight-bold').text(title).appendTo(cardBody);
+
+    $('<p>').addClass('card-text text-muted').text(sub_title).appendTo(cardBody);
+
+    const creator = $('<div>').addClass('creator d-flex align-items-center').appendTo(cardBody);
+    $('<img>').attr({'src': author_pic_url, 'alt': 'Author'}).css('width', '30px').addClass('rounded-circle').appendTo(creator);
+    $('<h6>').addClass('pl-3 m-0 main-color').text(author).appendTo(creator);
+
+    const info = $('<div>').addClass('info pt-3 d-flex justify-content-between').appendTo(cardBody);
+    const rating = $('<div>').addClass('rating d-flex').appendTo(info);
+    rating.append(stars);
+    $('<span>').addClass('main-color').text(duration).appendTo(info);
+
+    return cardContainer;
 }
 
-
-function fetchVideos() {
-    var loader = $('#loader');
+$(document).ready(function () {
+    const loader = $('.loader');
     loader.show();
 
     $.get("https://smileschool-api.hbtn.info/popular-tutorials", function (data) {
         loader.hide();
-        var carouselInner = $('#carouselExampleControls2 .carousel-inner');
-        carouselInner.empty();
+        $('.carousel-inner.carousel-popular').empty();
 
-        var slide = $('<div>').addClass('carousel-item active');
-        var row = $('<div>').addClass('row');
-        slide.append(row);
+        data.forEach((video, index) => {
+            const card = createCard(video);
+            $('.carousel-inner.carousel-popular').append(card);
+        });
 
-        for (var i = 0; i < data.length; i++) {
-            var cardCol = createVideoCard(data[i]);
-            row.append(cardCol);
-
-            if (i > 0 && (i + 1) % 4 === 0 && i !== data.length - 1) {
-                slide = $('<div>').addClass('carousel-item');
-                row = $('<div>').addClass('row');
-                slide.append(row);
-                carouselInner.append(slide);
-            }
-        }
-
-        carouselInner.append(slide);
+        $('.carousel-inner.carousel-popular').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            prevArrow: '.slick-prev',
+            nextArrow: '.slick-next',
+            responsive: [
+                {
+                    breakpoint: 769,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });
     });
-}
-
-$(document).ready(fetchVideos);
-
+});
